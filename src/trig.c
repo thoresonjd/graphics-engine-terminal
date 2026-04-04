af2d3a524e9bed1b0e616e449c85e73d7201baad
Justin Thoreson <thoresonjd@gmail.com>
2026-04-03 19:12:09 -0700

    Correct constant name representing 180 degrees

--- src/trig.c
+++ src/trig.c
@@ -7,12 +7,15 @@
 #include <trig.h>
 #include <math.h>
 
-static const float FULL_CIRCLE_DEGREES = 180.0f;
+/**
+ * @brief Angle of degrees denoting a half circle of rotation.
+ */
+static const float HALF_CIRCLE_DEGREES = 180.0f;
 
 float angle_radians(const float degrees) {
-	return (degrees / FULL_CIRCLE_DEGREES) * M_PI;
+	return (degrees / HALF_CIRCLE_DEGREES) * M_PI;
 }
 
 float angle_degrees(const float radians) {
-	return (radians / M_PI) * FULL_CIRCLE_DEGREES;
+	return (radians / M_PI) * HALF_CIRCLE_DEGREES;
 }
